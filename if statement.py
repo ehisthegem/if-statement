fd619d0f3178b8cb0f9e94641a9addb981147f14
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:50:12 +0100

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,36 @@
+#Python condtions and if statement 
+whole_numbers = 234
+decimals = 909.56
+
+if whole_numbers > decimals :
+    print('whole numbers')
+else :
+    print('decimals')
+
+rice = 234
+turkey = 234
+
+if rice > turkey:
+    print('rice')
+elif rice == turkey:
+    print('rice and turkey are the same values')
+else:
+    print('no value is equal')
+
+rice_2 = 786
+turkey_2 = 469
+
+if rice_2 < turkey_2:
+    print('rice is better')
+elif rice_2 != turkey_2:
+    print('none is better than the other')
+else:
+    print('turkey is better')
+
+rice_3 = 76
+turkey_3 = 46
+
+if rice_3 > turkey_3:
+    pass
+
+
